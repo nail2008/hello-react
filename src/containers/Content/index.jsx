718cf2101443fe99f6dba7c7e9709b5ac3330437
Juice Cheng <juice_cheng@163.com>
2016-05-03 10:20:20 +0800

    展示markdown内容

---
+++ src/containers/Content/index.jsx
@@ -0,0 +1,32 @@
+import React, {Component, PropTypes} from 'react';
+import {buildCommon} from '../../../scripts/utils'
+
+import './index.less';
+
+const propTypes = {
+
+};
+
+const contextTypes = {
+
+};
+
+class Content extends Component{
+  render(){
+  	mds = buildCommon("../../../docs/practice")
+
+    
+    return (
+      <div className="main-container">
+      	测试中
+      </div>
+    );
+  }
+}
+
+Content.propTypes = propTypes;
+
+Content.contextTypes = contextTypes;
+
+export default Content;
+
