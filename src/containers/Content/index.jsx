7d43eaeeadf2ed6f9e21765e548a2a08a02de1d2
JuiceChein <ziguinihao@aliyun.com>
2016-05-05 18:17:11 +0800

    添加对markdown 文档中组件的预览功能

--- src/containers/Content/index.jsx
+++ src/containers/Content/index.jsx
@@ -1,6 +1,9 @@
 import React, {Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
 import './index.less';
 import hljs from 'highlight.js';
+import {Button, Collapse} from 'antd';
+import BrowserDemo from '../../components/BrowserDemo';
 
 const propTypes = {
 
@@ -16,52 +19,116 @@ function mdLangToHljsLang(lang) {
     lang;
 }
 
+function parseChildren(object, index){
+  console.log(object);
+  console.log(index);
+  if (object === null) return;
+
+  if (React.isValidElement(object)) {
+    return React.cloneElement(object, { key: index });
+  }
+
+  if (typeof object === 'function') {
+    return object(React, ReactDOM, antd, antd);
+  }
+
+  if (typeof object === 'string') {
+    return <span key={index}>{ object }</span>;
+  }
+
+  const children = object.children;
+
+  if (object.type === 'html') {
+    return React.createElement('div', {
+      className: 'markdown',
+      key: index,
+      dangerouslySetInnerHTML: { __html: children }
+    });
+  }
+
+  if (/h[1-6]/i.test(object.type)) {
+    return React.createElement(object.type, {
+      key: index,
+      id: children,
+    }, [
+      <span dangerouslySetInnerHTML={{ __html: object.children }} />,
+    ]);
+  }
+
+  if (object.type === 'code') {
+    const highlightedCode = hljs.highlight(
+      object.props.lang.toLowerCase() === 'jsx' ?
+        'javascript' :
+        object.props.lang,
+      children
+    ).value;
+    return (
+      <div className="highlight" key={index}>
+        <pre>
+          <code className={object.props.lang}
+                dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+        </pre>
+      </div>
+    );
+  }
+
+  if (typeof children === 'string') {
+    console.log("children is string");
+    return React.createElement(object.type, {
+      key: index,
+      dangerouslySetInnerHTML: { __html: children }
+    });
+  }
+
+  return React.createElement(
+    object.type, { key: index },
+    children && children.map(parseChildren) // `hr` has no children
+  );
+}
+
 class Content extends Component{
   render(){
-    const content = require('antd-md!../../../static/docs/practice/cases.md');
-    console.log(content.description);
-    const title = content.intro[1][2][1][1].split(':')[1];
-
+    const demo = require('antd-md?demo!../../../static/docs/button/demo/button-group.md');
+    const _antd = require('antd');
+    demo.preview = demo.preview(React, ReactDOM, _antd, BrowserDemo);
+    const { id, meta, intro, code, preview, style, src,
+      expand, pathname } = demo;
+    const introChildren = intro.map(parseChildren);
+    const highlightedCode = hljs.highlight('javascript', code).value;
     return (
       <div className="main-container">
       	<article className="markdown">
-          <h1>
-            {title}
-          </h1>
-          {content.description.map(function(element, index){
-            let elementType = element[0];
-            let innerContent = element[2];
-            console.log(elementType);
-            if(/h[1-6]/i.test(elementType)){
-              return React.createElement(elementType, {
-                key: index,
-                id: innerContent,
-              }, [
-                <span key="title" dangerouslySetInnerHTML={{ __html: innerContent }} />,
-              ]);
-            }
-            if (typeof innerContent === 'string') {
-              return React.createElement(elementType, {
-                key: index,
-                dangerouslySetInnerHTML: { __html: innerContent }
-              });
-            }
+          <section className="code-box" id={id}>
+            <section className="code-box-demo">
+              { meta.iframe === 'true' ?
+                <iframe src={src} /> :
+                preview
+              }
+              {
+                !!style ?
+                  <style dangerouslySetInnerHTML={{ __html: style }} /> :
+                  null
+              }
+            </section>
+            <section className="code-box-meta markdown">
+              <div className="code-box-title">
+                { meta.chinese || meta.english }
+              </div>
+              <Collapse activeKey='code'>
+                <Collapse.Panel key="code" header={introChildren}>
+                  <div className="highlight">
+                    <pre>
+                      <code className="javascript" dangerouslySetInnerHTML={{
+                        __html: highlightedCode,
+                      }} />
+                    </pre>
+                  </div>
+                </Collapse.Panel>
+              </Collapse>
+            </section>
+          </section>
 
-            if (elementType === 'script') {
-              const highlightedCode = hljs.highlight(
-                mdLangToHljsLang("bash"),
-                innerContent
-              ).value;
-              return (
-                <div className="highlight" key={index}>
-                  <pre>
-                    <code className="bash"
-                      dangerouslySetInnerHTML={{ __html: highlightedCode }} />
-                  </pre>
-                </div>
-              );
-            }
-          })}
+
 
         </article>
       </div>
