5c116beee788ad74b4f25d8a06406cb42b06f5dd
zhulichao <zhulichao@cplh.com.cn>
2016-05-05 19:31:38 +0800

    demo中添加新组件

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
-import { Tabs  } from 'antd';
+import { Tabs ,Alert } from 'antd';
 
 import CardForm from '../containers/FormDemo/CardForm'
 import ListForm from '../containers/FormDemo/ListForm'
-
+import CalendarForm from '../containers/FormDemo/CalendarForm'
 const TabPane = Tabs.TabPane;
 
 
 
 const Tab = React.createClass({
-
 	render(){
 		const breadCrumb = this.props.breadCrumb;
-		console.log(breadCrumb);
 		const tabList = this.props.tabList.map(function(data, index) {
+			let showForm = null;
+			if(data.value == "CardForm"){
+				showForm = <CardForm breadCrumb={breadCrumb}/>;
+			}
+			if(data.value == "ListForm"){
+				showForm =  <ListForm/>;
+			}
+			if(data.value == "CalendarForm"){
+				showForm =  <CalendarForm/>;
+			}
 	     	return (
 				<TabPane tab={data.name} key={data.value}>
-					{data.value == "CardForm"?<CardForm breadCrumb={breadCrumb}/>:<ListForm/>}
+					{showForm}
 				</TabPane>
 			);
 		});
 		return (
-			
-			<Tabs id="tab" defaultActiveKey={this.props.defaultTab}  >
-				{tabList}
-		  	</Tabs>
+			<div>
+				<Tabs id="tab" defaultActiveKey={this.props.defaultTab}  >
+					{tabList}
+			  	</Tabs>
+		  	</div>
 		  	
   		)
 	}
